979e82fbf420468d870ccd697f318b63b74f01fc
Syakhisk Al Azmi <syakhisk.syari@gojek.com>
2024-05-13 14:37:33 +0700

    update

--- scripts/fuzzy-chooser.sh
+++ scripts/fuzzy-chooser.sh
@@ -13,14 +13,20 @@ _c_underline='\x1b[4m'
 
 current_session=$(tmux display -p '#{session_name}')
 current_window=$(tmux display -p '#{window_index}')
+current_pane=$(tmux display -p '#{pane_index}')
 
-tmux list-windows -a -F '#{session_name}|#{window_index}|#{window_name}' |
+# tmux list-windows -a -F '#{session_name}|#{window_index}|#{window_name}|#{pane_current_command}' |
+tmux list-panes -a -F '#{session_name}|#{window_index}|#{window_name}|#{pane_index}|#{pane_current_command}' |
 	while read -r row; do
 		session_name=$(echo "$row" | cut -d '|' -f 1)
 		window_index=$(echo "$row" | cut -d '|' -f 2)
 		window_name=$(echo "$row" | cut -d '|' -f 3)
+		pane_index=$(echo "$row" | cut -d '|' -f 4)
+		pane_command=$(echo "$row" | cut -d '|' -f 5)
 
-		if [[ "$session_name" = "$current_session" ]] && [[ "$window_index" = "$current_window" ]]; then
+		if [[ "$session_name" = "$current_session" ]] \
+      && [[ "$window_index" = "$current_window" ]] \
+      && [[ "$pane_index" = "$current_pane" ]]; then
       echo -ne "${_c_underline}"
 		fi
 
@@ -28,6 +34,7 @@ tmux list-windows -a -F '#{session_name}|#{window_index}|#{window_name}' |
 		echo -ne ":"
 		echo -ne "${_c_widx}${window_index}"
 		echo -ne "${_c_white} - ${window_name}"
+		echo -ne "[${pane_command}]"
 		echo -ne "${_c_reset}"
 
 		# if [[ "$session_name" = "$current_session" ]] && [[ "$window_index" = "$current_window" ]]; then
@@ -38,10 +45,12 @@ tmux list-windows -a -F '#{session_name}|#{window_index}|#{window_name}' |
 	done |
 	fzf \
 		--ansi \
+    --header="Choose pane: " \
 		--preview='tmux capture-pane -pe -t {1}' \
-		--preview-window=bottom,70% \
+		--preview-window=right,70% \
 		--bind 'ctrl-/:change-preview-window(hidden|)' |
 	sed -E 's/ -.*//' |
 	xargs -r tmux switch-client -t
 
+    # --bind 'ctrl-r:reload(tmux capture-pane -pe -t {1})' |
 # --preview='while true; do tmux capture-pane -pe -t {1}; clear; sleep 1; done' \
