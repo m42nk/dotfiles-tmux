2a334b84c1df6c2471462c9e409c0359129c407a
Syakhisk Al Azmi <mail.syakhisk@gmail.com>
2025-10-02 14:18:01 +0700

    cleanup flow

--- scripts/launcher-v2.py
+++ scripts/launcher-v2.py
@@ -29,31 +29,14 @@ TMP_MODE = os.path.join(TMP_DIR, "tmux-launcher-mode")
 def main():
     args = args_parse()
 
+    # Create the file with default mode if it doesn't exist
     if not os.path.exists(TMP_MODE):
-        f = open(TMP_MODE, "w")
-        f.write(MODE_DEFAULT)
-        f.close()
+        with open(TMP_MODE, "w") as f:
+            f.write(MODE_DEFAULT)
 
-    mode = args.mode or MODE_DEFAULT
-
-    if args.static:
-        f = open(TMP_MODE, "r")
-        mode = f.read().strip()
-        f.close()
-    else:
-        f = open(TMP_MODE, "w")
-        f.write(mode)
-        f.close()
-
-    if args.mode in ["next", "prev"]:
-        mode = get_next_mode(mode) if args.mode == "next" else get_prev_mode(mode)
-
-        f = open(TMP_MODE, "w")
-        f.write(mode)
-        f.close()
-
+    mode = build_mode(args)
     content = get_content(mode)
-    header = get_header(mode)
+    header = build_header(mode)
 
     if args.static:
         if args.static == "header":
@@ -62,7 +45,7 @@ def main():
             print(content)
         return
 
-    fuzzy_find(mode, content, header)
+    fuzzy_find(content, header)
 
 
 def args_parse():
@@ -71,7 +54,9 @@ def args_parse():
     parser.add_argument(
         "--static",
         type=str,
-        default="",
+        nargs="?",
+        const=True,
+        default=None,
         choices=["header", "content"],
         help="get static text only and skip fuzzy search",
     )
@@ -86,23 +71,6 @@ def args_parse():
     return parser.parse_args()
 
 
-def fuzzy_find(mode, content, header):
-    subprocess.run(
-        [
-            "fzf",
-            f"--listen={PORT}",
-            "--ansi",
-            "--header=" + header,
-            f"--bind={KEY_MODE_NEXT}:"
-            + f"execute-silent({SCRIPT_PATH} --static header --mode next)+reload({SCRIPT_PATH} --static content)+transform-header({SCRIPT_PATH} --static header)",
-            f"--bind={KEY_MODE_PREV}:"
-            + f"execute-silent({SCRIPT_PATH} --static header --mode prev)+reload({SCRIPT_PATH} --static content)+transform-header({SCRIPT_PATH} --static header)",
-        ],
-        input=content.encode(),
-        text=False,
-    )
-
-
 def get_content(mode):
     if mode == "all":
         return "hello from all"
@@ -114,20 +82,33 @@ def get_content(mode):
         raise ValueError(f"Unknown mode: {mode}")
 
 
-def get_header(current_mode):
+def fuzzy_find(content, header):
+    # TODO: handle output
+
+    subprocess.run(
+        [
+            "fzf",
+            f"--listen={PORT}",
+            "--ansi",
+            "--header=" + header,
+            f"--bind={KEY_MODE_NEXT}:execute-silent({SCRIPT_PATH} --static --mode next)"
+            f"+reload({SCRIPT_PATH} --static content)"
+            f"+transform-header({SCRIPT_PATH} --static header)",
+            f"--bind={KEY_MODE_PREV}:execute-silent({SCRIPT_PATH} --static --mode prev)"
+            f"+reload({SCRIPT_PATH} --static content)"
+            f"+transform-header({SCRIPT_PATH} --static header)",
+        ],
+        input=content.encode(),
+        text=False,
+    )
+
+
+def build_header(current_mode):
     header = f"Press {KEY_MODE_NEXT}/{KEY_MODE_PREV} to switch mode: "
-    return f"{header}\n{get_header_modes(current_mode)}"
+    return f"{header}\n{build_header_modes(current_mode)}"
 
 
-def get_next_mode(current_mode):
-    return MODES[(MODES.index(current_mode) + 1) % len(MODES)]
-
-
-def get_prev_mode(current_mode):
-    return MODES[(MODES.index(current_mode) - 1) % len(MODES)]
-
-
-def get_header_modes(current_mode):
+def build_header_modes(current_mode):
     c_bold = subprocess.getoutput("tput bold")
     c_reset = subprocess.getoutput("tput sgr0")
 
@@ -139,6 +120,42 @@ def get_header_modes(current_mode):
             header_modes.append(mode)
 
     return " ".join(header_modes)
+
+
+def get_next_mode(current_mode):
+    return MODES[(MODES.index(current_mode) + 1) % len(MODES)]
+
+
+def get_prev_mode(current_mode):
+    return MODES[(MODES.index(current_mode) - 1) % len(MODES)]
+
+
+def build_mode(args):
+    if args.static:
+        current_mode = get_stored_mode()
+    else:
+        current_mode = args.mode or MODE_DEFAULT
+        set_stored_mode(current_mode)
+
+    mode = current_mode
+    if args.mode in ["next", "prev"]:
+        mode = get_next_mode(mode) if args.mode == "next" else get_prev_mode(mode)
+        set_stored_mode(mode)
+
+    return mode
+
+
+def get_stored_mode():
+    if os.path.exists(TMP_MODE):
+        with open(TMP_MODE, "r") as f:
+            return f.read().strip()
+
+    return MODE_DEFAULT
+
+
+def set_stored_mode(mode):
+    with open(TMP_MODE, "w") as f:
+        f.write(mode)
 
 
 if __name__ == "__main__":
