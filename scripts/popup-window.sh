f25a41169a75d7913a9a63ee3a5e1192c2c28094
Syakhisk Al Azmi <mail.syakhisk@gmail.com>
2026-07-14 11:19:42 +0700

    update

--- scripts/popup-window.sh
+++ scripts/popup-window.sh
@@ -1,22 +1,33 @@
 #!/usr/bin/env bash
 
-SESSION_NAME="${SESSION_NAME:-scratch}"
+LOG_FILE="${TMUX_LOG_FILE:-${TMPDIR:-/tmp}/tmux-popup-window.log}"
+POPUP_SESSION_NAME="${SESSION_NAME:-scratch}"
+CURRENT_SESSION="${CURRENT_SESSION:-}"
 DIR="${DIR:-}"
 SHELL_CMD="${SHELL_CMD:-}"
 OPTS=${OPTS:-}
 
-if [ "$(tmux display-message -p -F "#{session_name}")" = "$SESSION_NAME" ];then
-    tmux detach-client
-    exit 0
+log() {
+  printf '%s [%s] %s\n' \
+    "$(date '+%Y-%m-%dT%H:%M:%S%z')" \
+    "$$" \
+    "$*" >>"$LOG_FILE"
+}
+
+log "started session=$POPUP_SESSION_NAME current=${CURRENT_SESSION:-<empty>} dir=${DIR:-<empty>} shell_cmd=${SHELL_CMD:-<empty>}"
+
+if [ "$CURRENT_SESSION" = "$POPUP_SESSION_NAME" ]; then
+  tmux detach-client -s "$POPUP_SESSION_NAME"
+  exit 0
 fi
 
 _OPTS=""
 
 if [ -n "$DIR" ]; then
-    _OPTS="$_OPTS -c $DIR"
+  _OPTS="$_OPTS -c $DIR"
 fi
 
-CMD="tmux attach -t ${SESSION_NAME} ${_OPTS} ${SHELL_CMD} || tmux new -s ${SESSION_NAME} ${_OPTS} ${SHELL_CMD}"
+CMD="tmux attach -t ${POPUP_SESSION_NAME} ${_OPTS} ${SHELL_CMD} || tmux new -s ${POPUP_SESSION_NAME} ${_OPTS} ${SHELL_CMD}"
 
 # Debug
 # tmux display-message "$CMD"
